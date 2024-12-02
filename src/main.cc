b3d201c7926da34b8bad71b73eec19a57149a11c
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-12-02 06:32:09 +0100

    add glfw

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,42 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int
-main(void)
+main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Use OpenGL version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Core profile
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);                       // Allow window resizing
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
